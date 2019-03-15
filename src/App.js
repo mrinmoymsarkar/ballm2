c154449e5ec49d2d9571ce3c3b3451ad15d885b0
Mrinmoy <mrinmoy.sarkar@techprimelab.com>
2019-03-15 21:06:04 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from "react";
+import MoveBall from "./MoveBall/MoveBall";
 
-class App extends Component {
+class App extends React.Component {
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return(
+        <div>
+          <div>
+            <MoveBall
+            radius = {12}
+            x = {100}
+            y = {60}
+            width = {480}
+            height = {320}
+            />
+          </div>
+        </div>
+    )
   }
 }
 
-export default App;
+
+export  default  App;
+
+
